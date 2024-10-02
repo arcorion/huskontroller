c51f6e9e0481f6ec5d31d60388f67df061921318
Christopher Lee Crader <ccrader@uw.edu>
2024-10-02 16:23:04 -0700

    Add logging to serial interface, improve comments

--- extron_serial.py
+++ extron_serial.py
@@ -1,29 +1,59 @@
+import logging
 import serial
 from serial.tools import list_ports
 
 class Extron:
     def __init__(self):
+        """
+        Initializes device - gets a list of all connected
+        serial devices, picking the first from the list and assuming
+        that's the Extron device. If multiple serial devices are
+        connected, modify this to account for the devices.
+        """
+        self.logger = logging.getLogger('extron_serial')
+        self.logger.setLevel(logging.DEBUG)
+        
+        file_handler = logging.FileHandler('logs/extron_serial.log')
+        file_handler.setLevel(logging.DEBUG)
+        
+        formatter = logging.Formatter(fmt='%(asctime)s - %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        self.logger.info('Extron serial device connection initializing...\n')
+        
         try:
             port_list = list_ports.comports()
             port_names = []
             for port in port_list:
                 port_names += port
             self.extron_device = serial.Serial('COM3', 9600)
-            print(f'Device {self.extron_device} created.')
+            self.logger.info(f'Device {self.extron_device} created.')
         except serial.SerialException:
-            print(f'Error opening connection to port: {port_list[0]}')
-        self.current_volume = -10
+            self.logger.error(f'Error opening connection to port: {port_list[0]}')
+        self.current_volume = -25
     
     def send_command(self, command):
-        print("Sent command: " + command)
-        #self.extron_device.write(command.encode())
+        """
+        Takes a command string and sends the command as an
+        encoded byte string to the Extron device.
+        """
+        self.logger.info(f'Sending command: {command}')
+        self.extron_device.write(command.encode())
     
     def select_input(self, input_device):
+        """
+        Takes the name of an input device 'podium', 'hdmi', 'usb-c', or 'vga'
+        and sends the associated command to the Extron switcher.
+        """
         inputs = {'podium': '1!', 'hdmi': '2!', 'usb-c': '3!', 'vga': '4!'}
         command = inputs[input_device]
         self.send_command(command)
     
     def freeze(self):
+        """
+        Sends the Extron command necessary for 
+        """
         self.send_command('1*1F')
         
     def unfreeze(self):
